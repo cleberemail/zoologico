b41d639486f8428d56713821c78b4f692cc2ca81
cleber <cleber.email@gmail.com>
2024-07-07 17:10:30 -0300

    na branch rota-caninos, adicionamos peixes e retiramos o espaço em branco de outras rotas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -15,41 +15,47 @@ class HelloController
     #[Route('/dinossauro')]
     function dinossauro(): Response
     {
-        return new Response('dinossauro ' . uniqid());
+        return new Response('dinossauro' . uniqid());
     }
 
     #[Route('/mamiferos')]
     function mamiferos(): Response
     {
-        return new Response('mamiferos ' . uniqid());
+        return new Response('mamiferos' . uniqid());
     }
 
     #[Route('/invertebrados')]
     function invertebrados(): Response
     {
-        return new Response('invertebrados ' . uniqid());
+        return new Response('invertebrados' . uniqid());
     }
     #[Route('/insetos')]
     function insetos(): Response
     {
-        return new Response('insetos ' . uniqid());
+        return new Response('insetos' . uniqid());
     }
 
     #[Route('/aves')]
     function aves(): Response
     {
-        return new Response('aves ' . uniqid());
+        return new Response('aves' . uniqid());
     }
 
     #[Route('/felinos')]
     function felinos(): Response
     {
-        return new Response('felinos ' . uniqid());
+        return new Response('felinos' . uniqid());
     }
 
     #[Route('/caninos')]
     function caninos(): Response
     {
-        return new Response('caninos ' . uniqid());
+        return new Response('caninos' . uniqid());
+    }
+
+    #[Route('/peixes')]
+    function peixes(): Response
+    {
+        return new Response('peixes' . uniqid());
     }
 }
